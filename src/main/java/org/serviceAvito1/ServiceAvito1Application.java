eaa9157f90d49c63ea96e357434eb751d6c33f64
ajddig <ajddig2013@gmail.com>
2016-11-02 10:11:18 +0000

    init project

---
+++ src/main/java/org/serviceAvito1/ServiceAvito1Application.java
@@ -0,0 +1,12 @@
+package org.serviceAvito1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAvito1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAvito1Application.class, args);
+	}
+}
